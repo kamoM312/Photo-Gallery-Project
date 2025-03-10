3d7e3a2435b289a0f1ff5b3dc6c6e25a03617db3
kamo312 <kamogelomalema1@gmail.com>
2025-03-10 13:21:28 -0400

    modal styling

--- app.js
+++ app.js
@@ -7,18 +7,18 @@ function createModal() {
                     <div class="modal_btns">
                         <div class="btn_previous">
                             <button>previous</button>
-                        </div">
+                        </div>
                         
                         <div class="btn_close">
-                            <button>close</button>
-                        </div">
-                        <div class="btn_next">
                             <button>next</button>
-                        </div">
+                        </div>
+                        <div class="btn_next">
+                            <button>close</button>
+                        </div>
                     </div>
                 </div>
                 <div class="modal_img">
-                    <img src="./assets/images/dolphins.jpg" alt="dolphin"/>
+                    <img class="modal_img_size" src="./assets/images/dolphins.jpg" alt="dolphin"/>
                 </div>
             </div>
         </div>

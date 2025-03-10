25df14c049cd969878613f7c75cf7343cb407bd1
kamo312 <kamogelomalema1@gmail.com>
2025-03-10 14:17:02 -0400

    modal styling

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function createModal() {
     document.querySelector("body").insertAdjacentHTML(
         'beforeend', `
-        <div class="modal_container">
+        <div class="modal_container" hidden="true">
             <div class="modal">
                 <div class="modal_overlay">
                     <div class="modal_btns">
@@ -19,6 +19,10 @@ function createModal() {
                 </div>
                 <div class="modal_img">
                     <img class="modal_img_size" src="./assets/images/dolphins.jpg" alt="dolphin"/>
+                    <img class="modal_img_size" src="./assets/images/elephant.jpg" alt="dolphin"/>
+                    <img class="modal_img_size" src="./assets/images/giraffes.jpg" alt="dolphin"/>
+                    <img class="modal_img_size" src="./assets/images/goat.jpg" alt="dolphin"/>
+                    <img class="modal_img_size" src="./assets/images/kangaroo.jpg" alt="dolphin"/>
                 </div>
             </div>
         </div>

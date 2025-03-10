e6bec6c9fec706a5a4139d717419501c01ea3ef9
kamo312 <kamogelomalema1@gmail.com>
2025-03-10 12:36:52 -0400

    createModal method correction

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 function createModal() {
-    document.querySelector("body").insertAdjacentElement(
+    document.querySelector("body").insertAdjacentHTML(
         'beforeend', `
         <div class="modal_container">
             <div class="modal">
                 <div class="modal_overlay">
-                    <div class="btn_previous"></div">
-                    <div class="btn_close"></div">
-                    <div class="btn_next"></div">
+                    <div class="modal_btns">
+                        <div class="btn_previous">
+                            <button>previous</button>
+                        </div">
+                        
+                        <div class="btn_close">
+                            <button>close</button>
+                        </div">
+                        <div class="btn_next">
+                            <button>next</button>
+                        </div">
+                    </div>
+                </div>
+                <div class="modal_img">
+                    <img src="./assets/images/dolphins.jpg" alt="dolphin"/>
                 </div>
             </div>
         </div>
-        `
-    )
-}
+        
+    `)
+    console.log("working")
+};
 
 createModal();
